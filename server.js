48e129a3668de877cc7b22f159d8486584c49178
Tayyab Mazhar <tayyabrana213@gmail.com>
2023-08-07 22:07:18 +0500

    Origin Removed

--- server.js
+++ server.js
@@ -25,12 +25,12 @@ app.use(cors({
 
 app.use(cookieParser('JOBAPI'))
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type Authorization')
-    next()
-})
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type Authorization')
+//     next()
+// })
 
 app.use(categoryRouter.getAllCategories)
 
