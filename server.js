fdf7f5d66a04daeb07498a9f37164165ae2f1e53
Tayyab Mazhar <tayyabrana213@gmail.com>
2023-08-07 21:59:43 +0500

    Port changed

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const categoryRouter = require('./controllers/category');
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
