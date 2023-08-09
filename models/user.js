ec5f43d455af710fa1e05bedfc48898c561fe5ee
Tayyab Mazhar <tayyabrana213@gmail.com>
2023-08-09 19:11:48 +0500

    User

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const db = require('../util/database')
+
+module.exports = class User {
+    constructor(id, name, username, email, password, phone, address, status) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.address = address;
+    }
+}
