443173a631cf39373bdb8252efd6e4af8b2b8c6a
mudassir <hiyfiycom@gmail.com>
2023-08-09 21:21:02 +0500

    cv builder

--- models/cv.js
+++ models/cv.js
@@ -1,11 +1,9 @@
 const db = require('../util/database')
 
 module.exports = class Country {
-    constructor(id, job, user, data, proposal) {
+    constructor(id, user, statement) {
         this.id = id;
-        this.job = job;
         this.user = user;
-        this.data = data;
-        this.proposal = proposal;
+        this.statement = statement;
     }
 }
