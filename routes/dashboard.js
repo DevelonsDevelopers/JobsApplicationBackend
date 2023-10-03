d240eb659fcaa3218a2c27a969124d9a673aa782
Tayyab Mazhar <tayyabrana213@gmail.com>
2023-10-03 16:10:03 +0500

    Fixed

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,10 +4,10 @@ const dashboardController = require('../controllers/dashboard')
 const router = express.Router();
 
 router.get('/api/dashboard/dashboard', dashboardController.getDashboard);
-router.get('/api/dashboard/companyDashboard', dashboardController.getCompanyDashboard);
+router.post('/api/dashboard/companyDashboard', dashboardController.getCompanyDashboard);
 router.get('/api/dashboard/pieChart', dashboardController.getPieChart);
 router.get('/api/dashboard/lineChart', dashboardController.getLineChart);
-router.get('/api/dashboard/companyLineChart', dashboardController.getCompanyLineChart);
+router.post('/api/dashboard/companyLineChart', dashboardController.getCompanyLineChart);
 router.get('/api/dashboard/barChart', dashboardController.getBarChart);
 router.get('/api/dashboard/transactions', dashboardController.getDashboardTransactions);
 router.get('/api/dashboard/reports', dashboardController.getDashboardReports);
