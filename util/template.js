6657129fda99cba363baf93e558595f089ac3c4b
Tayyab Mazhar <tayyabrana213@gmail.com>
2023-10-23 13:27:30 +0500

    Template

--- util/template.js
+++ util/template.js
@@ -19,64 +19,141 @@ function template(
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Curriculum Vitae</title>
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;1,100;1,200;1,300;1,400;1,500&display=swap"
+        rel="stylesheet">
 </head>
 
-<body style="font-family: 'Poppins', sans-serif; background-color: #f4f4f4; display: flex; justify-content: center; align-items: center; min-height: 100vh;">
+<style>
+    * {
+        font-family: 'Poppins', sans-serif;
+    }
 
-    <div class="cv-container" style="margin-top: 10px; max-width: 60%; width: 100%; padding: 3rem; border: 1px solid #e1e1e1; border-radius: 20px; background-color: #ffffff; box-shadow: 0 0 20px rgba(0, 0, 0, 0.1); margin-bottom: 10px;">
-        <h2 class="name-heading" style="text-align: center; font-size: 20px; font-weight: 600; color: #333333;">${name}</h2>
-        <h2 class="role-heading" style="text-align: center; font-size: 18px; font-weight: 400; margin-bottom: 2rem; color: #333333; letter-spacing: 5px;">${role}</h2>
-        <div class="contact-info" style="display: flex; font-size: 15px; font-weight: 400; justify-content: center; gap: 20px; margin-top: -20px; color: #555555;">
-            <h3>${phone}</h3>
-            <h3>${email}</h3>
-            <h3>${address}</h3>
-        </div>
-        <hr class="separator" style="margin-top: 2rem; border-top: 2px solid #e1e1e1;">
-        <p>${intro}</p>
-        <hr class="separator" style="margin-top: 2rem; border-top: 2px solid #e1e1e1;">
-        <h2 class="section-heading" style="text-align: center; font-size: 20px; font-weight: 600; margin-bottom: 2rem; color: #333333;">Key Skills</h2>
-        <div class="institute-details">
-            ${skills.map((value) => (
-        `  <h3>${value.skill}</h3>`
+    h2 {
+        font-weight: 400;
+        font-size: 12px;
+    }
+
+    h3 {
+        font-weight: 400;
+        font-size: 12px;
+    }
+    h4 {
+        font-weight: 400;
+        font-size: 12px;
+    }
+    p {
+        font-weight: 400;
+        font-size: 12px;
+    }
+
+
+
+
+</style>
+
+<body>
+    <div style="background-color: rgb(231, 231, 231); border: 1px solid gray; border-radius: 20px; padding: 4%; margin: 5%;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); ">
+        <center>
+            <h2>${name}</h2>
+            <h2>${role}</h2>
+            <div style="display: flex;justify-content: center; gap: 20px;margin-top: -20px;">
+                <h3>${phone}</h3>
+                <h3>${email}</h3>
+            </div>
+            <hr>
+            <h2>Full stack</h2>
+            <hr>
+        </center>
+        <p>${intro}
+        </p>
+        <hr>
+        <h2 style="text-align: center;">Key skills</h2>
+        <hr>
+        <center>
+            <div style="grid-template-columns: repeat(3,minmax(0, 1fr)); display: grid;">
+                ${skills.map((value) => (
+        `  <div style="grid-template-columns: repeat(3,minmax(0, 1fr)); display: grid;">
+                    <h4>${value.skill}</h4>
+                </div>`
     ))}
-        </div>
-        <hr class="separator" style="margin-top: 2rem; border-top: 2px solid #e1e1e1;">
-        <h2 class="section-heading" style="text-align: center; font-size: 20px; font-weight: 600; margin-bottom: 2rem; color: #333333;">Employment History</h2>
+            </div>
+        </center>
+        <hr>
+        <h2 style="text-align: center;">Employment History</h2>
+        <hr>
         <div>
             ${careers.map((value) => (
-        `  <div class="job-item">
-                <div class="institute-details">
-                    <h3>${value.job} <span>|</span> ${value.timeperiod}</h3>
-                    <h3>Company: ${value.company}</h3>
-                    <h3>Address: ${value.address}</h3>
-                    <h3 style="text-align: left;">Phone: ${value.phone}</h3>
+        `<div>
+                <div style="display: flex;gap: 20px;">
+                    <h3>${value.job}</h3>
+                    <h3>|</h3>
+                    <h3>${value.timeperiod}</h3>
                 </div>
+                <div style="display: flex;margin-top: -20px;gap: 20px;">
+                    <h3>Company</h3>
+                    <h3>:</h3>
+                    <h3>${value.company}</h3>
+                </div>
+                <div style="display: flex;margin-top: -20px;gap: 20px;">
+                    <h3>Address</h3>
+                    <h3>:</h3>
+                    <h3>${value.address}</h3>
+                </div>
+                <h3 style="text-align: right;">Phone:${value.phone}</h3>
             </div>`
     ))}
+
+            <hr>
         </div>
-        <hr class="separator" style="margin-top: 2rem; border-top: 2px solid #e1e1e1;">
-        <h2 class="section-heading" style="text-align: center; font-size: 20px; font-weight: 600; margin-bottom: 2rem; color: #333333;">Qualifications</h2>
-        <div class="institute-details">
+
+        <h2 style="text-align: center;">Qualification</h2>
+        <hr>
+        <div>
             ${educations.map((value) => (
-        `  <h3>${value.qualification} <span>|</span> ${value.timeperiod}</h3>`
+        `<div>
+                <div style="display: flex;gap: 20px;">
+                    <h3>${value.qualification}</h3>
+                    <h3>|</h3>
+                    <h3>${value.timeperiod}</h3>
+                </div>
+
+                <h3 style="text-align: right;">${value.address}</h3>
+
+            </div>`
     ))}
+            <hr>
         </div>
-        <hr class="separator" style="margin-top: 2rem; border-top: 2px solid #e1e1e1;">
-        <h2 class="section-heading" style="text-align: center; font-size: 20px; font-weight: 600; margin-bottom: 2rem; color: #333333;">Training Courses</h2>
-        <div class="institute-details">
+        <h2 style="text-align: center;">Training Courses</h2>
+        <hr>
+        <div>
             ${courses.map((value) => (
-        `  <h3>${value.course} <span>|</span> ${value.timeperiod}</h3>`
+        `<div>
+                <div style="display: flex;gap: 20px;">
+                    <h3>${value.course}</h3>
+                    <h3>|</h3>
+                    <h3>${value.timeperiod}</h3>
+                </div>
+
+                <h3 style="text-align: right;">${value.address}</h3>
+            </div>`
     ))}
+            <hr>
         </div>
-        <hr class="separator" style="margin-top: 2rem; border-top: 2px solid #e1e1e1;">
-        <h2 class="section-heading" style="text-align: center; font-size: 20px; font-weight: 600; margin-bottom: 2rem; color: #333333;">Interests</h2>
-        <div class="institute-details">
-            ${interests.map((value) => (
-        `  <h3>${value.interest}</h3>`
+        <h2 style="text-align: center;">Interest</h2>
+        <hr>
+        <center>
+            <div style="grid-template-columns: repeat(3,minmax(0, 1fr)); display: grid;">
+                ${interests.map((value) => (
+        ` <h4>${value.interest}</h4>`
     ))}
-        </div>
+            </div>
+        </center>
     </div>
+
 
 </body>
 
