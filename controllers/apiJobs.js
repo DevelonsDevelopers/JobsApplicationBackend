ea1bb580a2efe1025c592239dd23a0101e707d20
awais123ahmad <awaisahmadajk@gmail.com>
2024-01-29 18:05:29 +0500

    apiJobsFetched successfully

--- controllers/apiJobs.js
+++ controllers/apiJobs.js
@@ -5,9 +5,9 @@ const apiJobsRouter = require("../routes/apiJobs");
 exports.getAllApiJobs = async (req, res, next) => {
     try {
         const [apiJobs] = await ApiJobs.fetchAll()
-        res.status(200).json({ "responseCode": 200, "message": "API Jobs fetched successfully", data: apiJobs});
+        res.status(200).json({ "responseCode": 200, "message": "API Jobs fetched successfully", data: apiJobs });
     } catch (error) {
-        if (!error.statusCode){
+        if (!error.statusCode) {
             error.statusCode = 500
         }
         next(error)
@@ -17,9 +17,9 @@ exports.getAllApiJobs = async (req, res, next) => {
 exports.getAllApiJobsData = async (req, res, next) => {
     try {
         const [apiJobs] = await ApiJobs.fetchAllJobs(req.body)
-        res.status(200).json({ "responseCode": 200, "message": "API Jobs fetched successfully", data: apiJobs});
+        res.status(200).json({ "responseCode": 200, "message": "API Jobs fetched successfully", data: apiJobs });
     } catch (error) {
-        if (!error.statusCode){
+        if (!error.statusCode) {
             error.statusCode = 500
         }
         next(error)
@@ -29,9 +29,9 @@ exports.getAllApiJobsData = async (req, res, next) => {
 exports.getRecentApiJobsData = async (req, res, next) => {
     try {
         const [apiJobs] = await ApiJobs.fetchRecentJobs(req.body)
-        res.status(200).json({ "responseCode": 200, "message": "API Jobs fetched successfully", data: apiJobs});
+        res.status(200).json({ "responseCode": 200, "message": "API Jobs fetched successfully", data: apiJobs });
     } catch (error) {
-        if (!error.statusCode){
+        if (!error.statusCode) {
             error.statusCode = 500
         }
         next(error)
@@ -41,9 +41,9 @@ exports.getRecentApiJobsData = async (req, res, next) => {
 exports.getJob = async (req, res, next) => {
     try {
         const [[apiJobs]] = await ApiJobs.fetchByID(req.body)
-        res.status(200).json({ "responseCode": 200, "message": "API Job fetched successfully", data: apiJobs});
+        res.status(200).json({ "responseCode": 200, "message": "API Job fetched successfully", data: apiJobs });
     } catch (error) {
-        if (!error.statusCode){
+        if (!error.statusCode) {
             error.statusCode = 500
         }
         next(error)
@@ -53,9 +53,9 @@ exports.getJob = async (req, res, next) => {
 exports.addApiJobs = async (req, res, next) => {
     try {
         const [apiJobs] = await ApiJobs.post(req.body)
-        res.status(200).json({ "responseCode": 200, "message": "API Jobs added successfully", data: apiJobs});
+        res.status(200).json({ "responseCode": 200, "message": "API Jobs added successfully", data: apiJobs });
     } catch (error) {
-        if (!error.statusCode){
+        if (!error.statusCode) {
             error.statusCode = 500
         }
         next(error)
@@ -67,23 +67,23 @@ exports.australiaJobs = async (req, res, next) => {
         method: 'get',
         maxBodyLength: Infinity,
         url: 'http://public.api.careerjet.net/search?keywords&location=australia&user_ip=11.22.33.44&user_agent=Mozilla/5.0 (Windows NT 6.3; WOW64; rv:37.0) Gecko/20100101 Firefox/37.0&affid=213e213hd12344552',
-        headers: { }
+        headers: {}
     };
 
     axios.request(config)
         .then(async (response) => {
-            console.log(JSON.stringify(response.data));
-            const array = response.data.jobs
-            for (let i = 0; i < array.length; i++) {
-                const title = array[i].title;
-                const description = array[i].description;
-                const locations = array[i].locations;
-                const site = array[i].site;
-                const url = array[i].url;
-                const date = array[i].date;
-                const company = array[i].company;
-                const salary = array[i].salary;
-                await ApiJobs.post({
+            // console.log(JSON.stringify(response.data));
+            const data = response.data.jobs;  
+            for (let i = 0; i < data.length; i++) {
+                const title = data[i].title;
+                const description = data[i].description;
+                const locations = data[i].locations;
+                const site = data[i].site;
+                const url = data[i].url;
+                const date = data[i].date;
+                const company = data[i].company;
+                const salary = data[i].salary;
+                await ApiJobs.post('/jobs', { 
                     title: title,
                     description: description,
                     locations: locations,
@@ -92,9 +92,9 @@ exports.australiaJobs = async (req, res, next) => {
                     date: date,
                     company: company,
                     salary: salary
-                })
+                });
             }
-            res.status(200).json({ "responseCode": 200, "message": "API Jobs added successfully", data: null});
+            res.status(200).json({ "responseCode": 200, "message": "API Jobs fetched successfully", data: data });
         })
         .catch((error) => {
             console.log(error);
@@ -106,7 +106,7 @@ exports.newzealandJobs = async (req, res, next) => {
         method: 'get',
         maxBodyLength: Infinity,
         url: 'http://public.api.careerjet.net/search?keywords&location=new zealand&user_ip=11.22.33.44&user_agent=Mozilla/5.0 (Windows NT 6.3; WOW64; rv:37.0) Gecko/20100101 Firefox/37.0&affid=213e213hd12344552',
-        headers: { }
+        headers: {}
     };
 
     axios.request(config)
@@ -133,7 +133,7 @@ exports.newzealandJobs = async (req, res, next) => {
                     salary: salary
                 })
             }
-            res.status(200).json({ "responseCode": 200, "message": "API Jobs added successfully", data: null});
+            res.status(200).json({ "responseCode": 200, "message": "API Jobs added successfully", data: null });
         })
         .catch((error) => {
             console.log(error);
